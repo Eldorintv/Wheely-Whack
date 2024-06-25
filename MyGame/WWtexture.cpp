4e357a74259eb09cc47833e2238ba5df37ed10e0
Mario Zugec <a12035412@unet.univie.ac.at>
2024-06-25 18:24:10 +0200

    Splitted WWtexture into header and source

---
+++ MyGame/WWtexture.cpp
@@ -0,0 +1,150 @@
+#include "WWtexture.h"
+
+
+Texture Texture::Load(const char* path) {
+    Texture texture;
+    texture.createTextureImage(path);
+    texture.createTextureImageView();
+    texture.createTextureSampler();
+
+    return texture;
+}
+
+Texture Texture::LoadCubeMap(const char* front, const char* back, const char* up, const char* down, const char* right, const char* left) {
+    Texture texture;
+
+    std::vector<const char* > faces;
+    faces.push_back(front); faces.push_back(back); faces.push_back(up); faces.push_back(down); faces.push_back(right); faces.push_back(left);
+
+    texture.createTextureImageCubeMap(faces);
+    texture.createTextureImageView(6);
+    texture.createTextureSampler(true);
+
+    return texture;
+}
+
+void Texture::Destroy() {
+    vkDestroySampler(device, textureSampler, nullptr);
+    vkDestroyImageView(device, textureImageView, nullptr);
+
+    vkDestroyImage(device, textureImage, nullptr);
+    vkFreeMemory(device, textureImageMemory, nullptr);
+}
+
+void Texture::createTextureImage(const char* path) {
+    int texWidth, texHeight, texChannels;
+
+    stbi_uc* pixels = stbi_load(std::string(path).c_str(), &texWidth, &texHeight, &texChannels, STBI_rgb_alpha);
+    VkDeviceSize imageSize = static_cast<VkDeviceSize>(texWidth) * texHeight * 4;
+
+    if (!pixels) {
+        throw std::runtime_error("failed to load texture image!");
+    }
+
+    VkBuffer stagingBuffer;
+    VkDeviceMemory stagingBufferMemory;
+    createBuffer(imageSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, stagingBuffer, stagingBufferMemory);
+
+    void* data;
+    vkMapMemory(device, stagingBufferMemory, 0, imageSize, 0, &data);
+    memcpy(data, pixels, static_cast<size_t>(imageSize));
+    vkUnmapMemory(device, stagingBufferMemory);
+
+    stbi_image_free(pixels);
+
+    createImage(texWidth, texHeight, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_TILING_OPTIMAL, VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, textureImage, textureImageMemory);
+
+    transitionImageLayout(textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
+    copyBufferToImage(stagingBuffer, textureImage, static_cast<uint32_t>(texWidth), static_cast<uint32_t>(texHeight));
+    transitionImageLayout(textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
+
+    vkDestroyBuffer(device, stagingBuffer, nullptr);
+    vkFreeMemory(device, stagingBufferMemory, nullptr);
+}
+
+std::vector<stbi_uc*> Texture::loadCubeMapImages(std::vector<const char*>& paths, int& texWidth, int& texHeight, int& texChannels) {
+    std::vector<stbi_uc*> cubeMapImages;
+
+    for (const auto& face : paths) {
+        stbi_uc* pixels = stbi_load(std::string(face).c_str(), &texWidth, &texHeight, &texChannels, STBI_rgb_alpha);
+        if (!pixels) {
+            throw std::runtime_error("failed to load texture image!" + *face);
+        }
+        cubeMapImages.push_back(pixels);
+    }
+
+    return cubeMapImages;
+}
+
+void Texture::createTextureImageCubeMap(std::vector<const char*>& paths) {
+    int texWidth, texHeight, texChannels;
+
+    std::vector<stbi_uc*> cubeMapImages = loadCubeMapImages(paths, texWidth, texHeight, texChannels);
+
+    VkDeviceSize imageSize = static_cast<VkDeviceSize>(texWidth) * texHeight * 4;
+    VkDeviceSize cubeImageSize = imageSize * 6;
+
+    VkBuffer stagingBuffer;
+    VkDeviceMemory stagingBufferMemory;
+    createBuffer(cubeImageSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, stagingBuffer, stagingBufferMemory);
+
+    void* data;
+    vkMapMemory(device, stagingBufferMemory, 0, cubeImageSize, 0, &data);
+    VkDeviceSize offset = 0;
+    for (const auto& pixels : cubeMapImages) {
+        memcpy(static_cast<char*>(data) + offset, pixels, static_cast<size_t>(imageSize));
+        offset += imageSize;
+    }
+    vkUnmapMemory(device, stagingBufferMemory);
+
+    for (auto& pixels : cubeMapImages) {
+        stbi_image_free(pixels);
+    }
+
+    createCubeMapImage(texWidth, texHeight, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_TILING_OPTIMAL, VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, textureImage, textureImageMemory);
+
+    transitionImageLayout(textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, 6);
+    copyBufferToImage(stagingBuffer, textureImage, static_cast<uint32_t>(texWidth), static_cast<uint32_t>(texHeight), 6);
+    transitionImageLayout(textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL, 6);
+
+    vkDestroyBuffer(device, stagingBuffer, nullptr);
+    vkFreeMemory(device, stagingBufferMemory, nullptr);
+}
+
+void Texture::createTextureImageView(uint32_t layerCount) {
+    textureImageView = createImageView(textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_ASPECT_COLOR_BIT, layerCount);
+}
+
+void Texture::createTextureSampler(bool isCube) {
+    VkPhysicalDeviceProperties properties{};
+    vkGetPhysicalDeviceProperties(physicalDevice, &properties);
+
+    VkSamplerCreateInfo samplerInfo{};
+    samplerInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
+    samplerInfo.magFilter = VK_FILTER_LINEAR;
+    samplerInfo.minFilter = VK_FILTER_LINEAR;
+    if (!isCube) {
+        samplerInfo.addressModeU = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+        samplerInfo.addressModeV = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+        samplerInfo.addressModeW = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+        samplerInfo.anisotropyEnable = VK_TRUE;
+    }
+    else {
+        samplerInfo.addressModeU = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE;
+        samplerInfo.addressModeV = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE;
+        samplerInfo.addressModeW = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE;
+        samplerInfo.anisotropyEnable = VK_FALSE;
+    }
+
+    samplerInfo.maxAnisotropy = properties.limits.maxSamplerAnisotropy;
+    samplerInfo.borderColor = VK_BORDER_COLOR_INT_OPAQUE_BLACK;
+    samplerInfo.unnormalizedCoordinates = VK_FALSE;
+    samplerInfo.compareEnable = VK_FALSE;
+    samplerInfo.compareOp = VK_COMPARE_OP_ALWAYS;
+    samplerInfo.mipmapMode = VK_SAMPLER_MIPMAP_MODE_LINEAR;
+
+    if (vkCreateSampler(device, &samplerInfo, nullptr, &textureSampler) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create texture sampler!");
+    }
+}
+
--- MyGame/WWtexture.h
+++ MyGame/WWtexture.h
@@ -11,152 +11,21 @@ public:
     VkSampler textureSampler;
 
 
-    static Texture Load(const char* path) {
-        Texture texture;
-        texture.createTextureImage(path);
-        texture.createTextureImageView();
-        texture.createTextureSampler();
+    static Texture Load(const char* path);
 
-        return texture;
-    }
+    static Texture LoadCubeMap(const char* front, const char* back, const char* up, const char* down, const char* right, const char* left);
 
-    static Texture LoadCubeMap(const char* front, const char* back, const char* up, const char* down, const char* right, const char* left) {
-        Texture texture;
-
-        std::vector<const char* > faces;
-        faces.push_back(front); faces.push_back(back); faces.push_back(up); faces.push_back(down); faces.push_back(right); faces.push_back(left);
-
-        texture.createTextureImageCubeMap(faces);
-        texture.createTextureImageView(6);
-        texture.createTextureSampler(true);
-
-        return texture;
-    }
-
-    void Destroy() {
-        vkDestroySampler(device, textureSampler, nullptr);
-        vkDestroyImageView(device, textureImageView, nullptr);
-
-        vkDestroyImage(device, textureImage, nullptr);
-        vkFreeMemory(device, textureImageMemory, nullptr);
-    }
+    void Destroy();
 private:
-    void createTextureImage(const char* path) {
-        int texWidth, texHeight, texChannels;
+    void createTextureImage(const char* path);
 
-        stbi_uc* pixels = stbi_load(std::string(path).c_str(), &texWidth, &texHeight, &texChannels, STBI_rgb_alpha);
-        VkDeviceSize imageSize = static_cast<VkDeviceSize>(texWidth) * texHeight * 4;
+    std::vector<stbi_uc*> loadCubeMapImages(std::vector<const char*>& paths, int& texWidth, int& texHeight, int& texChannels);
 
-        if (!pixels) {
-            throw std::runtime_error("failed to load texture image!");
-        }
+    void createTextureImageCubeMap(std::vector<const char*>& paths);
 
-        VkBuffer stagingBuffer;
-        VkDeviceMemory stagingBufferMemory;
-        createBuffer(imageSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, stagingBuffer, stagingBufferMemory);
+    void createTextureImageView(uint32_t layerCount = 1);
 
-        void* data;
-        vkMapMemory(device, stagingBufferMemory, 0, imageSize, 0, &data);
-        memcpy(data, pixels, static_cast<size_t>(imageSize));
-        vkUnmapMemory(device, stagingBufferMemory);
-
-        stbi_image_free(pixels);
-
-        createImage(texWidth, texHeight, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_TILING_OPTIMAL, VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, textureImage, textureImageMemory);
-
-        transitionImageLayout(textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
-        copyBufferToImage(stagingBuffer, textureImage, static_cast<uint32_t>(texWidth), static_cast<uint32_t>(texHeight));
-        transitionImageLayout(textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
-
-        vkDestroyBuffer(device, stagingBuffer, nullptr);
-        vkFreeMemory(device, stagingBufferMemory, nullptr);
-    }
-
-    std::vector<stbi_uc*> loadCubeMapImages(std::vector<const char*>& paths, int& texWidth, int& texHeight, int& texChannels) {
-        std::vector<stbi_uc*> cubeMapImages;
-        
-        for (const auto& face : paths) {
-            stbi_uc* pixels = stbi_load(std::string(face).c_str(), &texWidth, &texHeight, &texChannels, STBI_rgb_alpha);
-            if (!pixels) {
-                throw std::runtime_error("failed to load texture image!" + *face);
-            }
-            cubeMapImages.push_back(pixels);
-        }
-        
-        return cubeMapImages;
-    }
-
-    void createTextureImageCubeMap(std::vector<const char*>& paths) {
-        int texWidth, texHeight, texChannels;
-
-        std::vector<stbi_uc*> cubeMapImages = loadCubeMapImages(paths, texWidth, texHeight, texChannels);
-
-        VkDeviceSize imageSize = static_cast<VkDeviceSize>(texWidth) * texHeight * 4;
-        VkDeviceSize cubeImageSize = imageSize * 6;
-
-        VkBuffer stagingBuffer;
-        VkDeviceMemory stagingBufferMemory;
-        createBuffer(cubeImageSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, stagingBuffer, stagingBufferMemory);
-
-        void* data;
-        vkMapMemory(device, stagingBufferMemory, 0, cubeImageSize, 0, &data);
-        VkDeviceSize offset = 0;
-        for (const auto& pixels : cubeMapImages) {
-            memcpy(static_cast<char*>(data) + offset, pixels, static_cast<size_t>(imageSize));
-            offset += imageSize;
-        }
-        vkUnmapMemory(device, stagingBufferMemory);
-
-        for (auto& pixels : cubeMapImages) {
-            stbi_image_free(pixels);
-        }
-
-        createCubeMapImage(texWidth, texHeight, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_TILING_OPTIMAL, VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, textureImage, textureImageMemory);
-
-        transitionImageLayout(textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, 6);
-        copyBufferToImage(stagingBuffer, textureImage, static_cast<uint32_t>(texWidth), static_cast<uint32_t>(texHeight), 6);
-        transitionImageLayout(textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL, 6);
-
-        vkDestroyBuffer(device, stagingBuffer, nullptr);
-        vkFreeMemory(device, stagingBufferMemory, nullptr);
-    }
-
-    void createTextureImageView(uint32_t layerCount = 1) {
-        textureImageView = createImageView(textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_ASPECT_COLOR_BIT, layerCount);
-    }
-
-    void createTextureSampler(bool isCube = false) {
-        VkPhysicalDeviceProperties properties{};
-        vkGetPhysicalDeviceProperties(physicalDevice, &properties);
-
-        VkSamplerCreateInfo samplerInfo{};
-        samplerInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
-        samplerInfo.magFilter = VK_FILTER_LINEAR;
-        samplerInfo.minFilter = VK_FILTER_LINEAR;
-        if (!isCube) {
-            samplerInfo.addressModeU = VK_SAMPLER_ADDRESS_MODE_REPEAT;
-            samplerInfo.addressModeV = VK_SAMPLER_ADDRESS_MODE_REPEAT;
-            samplerInfo.addressModeW = VK_SAMPLER_ADDRESS_MODE_REPEAT;
-            samplerInfo.anisotropyEnable = VK_TRUE;
-        }
-        else {
-            samplerInfo.addressModeU = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE;
-            samplerInfo.addressModeV = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE;
-            samplerInfo.addressModeW = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE;
-            samplerInfo.anisotropyEnable = VK_FALSE;
-        }
-        
-        samplerInfo.maxAnisotropy = properties.limits.maxSamplerAnisotropy;
-        samplerInfo.borderColor = VK_BORDER_COLOR_INT_OPAQUE_BLACK;
-        samplerInfo.unnormalizedCoordinates = VK_FALSE;
-        samplerInfo.compareEnable = VK_FALSE;
-        samplerInfo.compareOp = VK_COMPARE_OP_ALWAYS;
-        samplerInfo.mipmapMode = VK_SAMPLER_MIPMAP_MODE_LINEAR;
-
-        if (vkCreateSampler(device, &samplerInfo, nullptr, &textureSampler) != VK_SUCCESS) {
-            throw std::runtime_error("failed to create texture sampler!");
-        }
-    }
+    void createTextureSampler(bool isCube = false);
 };
 
 #endif
